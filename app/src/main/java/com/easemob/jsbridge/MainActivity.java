dbf302cad538a8a459c66672ceaeba17130824f4
liyuzhao <lyuzhao@sina.com>
2019-05-17 20:38:37 +0800

    update impl to androidx

--- app/src/main/java/com/easemob/jsbridge/MainActivity.java
+++ app/src/main/java/com/easemob/jsbridge/MainActivity.java
@@ -1,7 +1,10 @@
 package com.easemob.jsbridge;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.easemob.test.R;
 
 public class MainActivity extends AppCompatActivity {
 
